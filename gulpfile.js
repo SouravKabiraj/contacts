69e9fd206afdae8cb909f155e39c5026be78247a
Sourav Kabiraj <sourav.kabiraj@outlook.com>
2019-09-21 01:28:45 +0530

    #021 | Sourav | Changed gulp test script.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
+const mocha = require('gulp-mocha');
 const gulp = require('gulp');
 const run = require('gulp-run');
 const zip = require('gulp-zip');
-const tslint = require('gulp-tslint');
 
+const tslint = require('gulp-tslint');
 const clean = function () {
     const del = require('del');
     return del(['build']);
-};
 
+};
 const install = function (done) {
     run('npm prune').exec(err => {
         if (err) {
@@ -20,24 +21,25 @@ const install = function (done) {
             done();
         });
     });
+
 };
 
-const test = function (done) {
-    run('npm test').exec(err => {
-        if (err) {
-            console.log(err);
-        }
-        done();
-    });
+const test = function () {
+    return gulp.src('*/**/*.spec.ts')
+        .pipe(mocha({
+            reporter: 'nyan',
+            require: ['ts-node/register'],
+            exit: true
+        }));
 };
-
 const assemble = function () {
+
     return gulp.src(['**/*', '!node_module/**', 'package-lock.json', '!.git/**', '!.idea/**'])
         .pipe(zip('contacts.zip'))
         .pipe(gulp.dest('compressed/dist/'))
 };
-
 const lint = function () {
+
     return gulp.src(['**/*.ts', '!node_modules/**'])
         .pipe(tslint({
             formatter: 'stylish'
@@ -46,6 +48,9 @@ const lint = function () {
             emitError: false
         }))
 };
-
 gulp.task('build', gulp.series(clean, lint, install, test, assemble));
+
 gulp.task('clean', clean);
+gulp.task('install', install);
+gulp.task('assemble', assemble);
+gulp.task('test', test);
